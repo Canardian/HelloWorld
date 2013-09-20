b6531012dcd9a5a4a5bb32f967a6239a6c0ce604
Canardian <mika@siipi.com>
2013-09-20 16:09:30 +0300

    moved triangle code to function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <GLFW/glfw3.h>
 
+void DrawTriangle()
+{
+	glBegin(GL_TRIANGLES);
+	glColor3f(1.f, 0.f, 0.f);
+	glVertex3f(-0.6f, -0.4f, 0.f);
+	glColor3f(0.f, 1.f, 0.f);
+	glVertex3f(0.6f, -0.4f, 0.f);
+	glColor3f(0.f, 0.f, 1.f);
+	glVertex3f(0.f, 0.6f, 0.f);
+	glEnd();
+}
+
 int main()
 {
     GLFWwindow* window;
@@ -18,10 +30,10 @@ int main()
     glfwMakeContextCurrent(window);
     while(running)
     {
-        frame++;
+		frame++;
 
-         float ratio;
-        int width, height;
+		float ratio;
+		int width, height;
 
         glfwGetFramebufferSize(window, &width, &height);
         ratio = width / (float) height;
@@ -32,25 +44,18 @@ int main()
 		glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
         glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
         glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.6f, 0.f);
-        glEnd();
+
+		DrawTriangle();
+
         glfwSwapBuffers(window);
         glfwPollEvents();
-
         // exit if ESC was pressed or window was closed
         running = !glfwWindowShouldClose(window);
     }
-
     glfwTerminate();
-
     return 0;
 }
